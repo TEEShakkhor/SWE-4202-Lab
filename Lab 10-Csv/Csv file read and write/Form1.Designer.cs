392845ec957f05502194a5780d6c48fc88e46800
TEEShakkhor <95173135+TEEShakkhor@users.noreply.github.com>
2022-03-17 23:22:23 +0600

    Add files via upload

---
+++ Lab 10-Csv/Csv file read and write/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Csv_file_read_and_write
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.Phone_Box = new System.Windows.Forms.Label();
+            this.Phone1_Box = new System.Windows.Forms.Label();
+            this.Zip_Box = new System.Windows.Forms.Label();
+            this.State_Box = new System.Windows.Forms.Label();
+            this.County_Box = new System.Windows.Forms.Label();
+            this.City_Box = new System.Windows.Forms.Label();
+            this.Address_Box = new System.Windows.Forms.Label();
+            this.Name_Box = new System.Windows.Forms.Label();
+            this.searchbox = new System.Windows.Forms.TextBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ListBox
+            // 
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.ItemHeight = 16;
+            this.ListBox.Location = new System.Drawing.Point(12, 22);
+            this.ListBox.Name = "ListBox";
+            this.ListBox.Size = new System.Drawing.Size(523, 436);
+            this.ListBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(720, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(720, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(720, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(720, 268);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 16);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "City";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(720, 306);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "County";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(720, 340);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "State";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(720, 442);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 16);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Phone";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(720, 415);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 16);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Phone 1";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(720, 374);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(26, 16);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Zip";
+            // 
+            // Phone_Box
+            // 
+            this.Phone_Box.AutoSize = true;
+            this.Phone_Box.Location = new System.Drawing.Point(802, 442);
+            this.Phone_Box.Name = "Phone_Box";
+            this.Phone_Box.Size = new System.Drawing.Size(0, 16);
+            this.Phone_Box.TabIndex = 17;
+            // 
+            // Phone1_Box
+            // 
+            this.Phone1_Box.AutoSize = true;
+            this.Phone1_Box.Location = new System.Drawing.Point(802, 415);
+            this.Phone1_Box.Name = "Phone1_Box";
+            this.Phone1_Box.Size = new System.Drawing.Size(0, 16);
+            this.Phone1_Box.TabIndex = 16;
+            // 
+            // Zip_Box
+            // 
+            this.Zip_Box.AutoSize = true;
+            this.Zip_Box.Location = new System.Drawing.Point(802, 374);
+            this.Zip_Box.Name = "Zip_Box";
+            this.Zip_Box.Size = new System.Drawing.Size(0, 16);
+            this.Zip_Box.TabIndex = 15;
+            // 
+            // State_Box
+            // 
+            this.State_Box.AutoSize = true;
+            this.State_Box.Location = new System.Drawing.Point(802, 340);
+            this.State_Box.Name = "State_Box";
+            this.State_Box.Size = new System.Drawing.Size(0, 16);
+            this.State_Box.TabIndex = 14;
+            // 
+            // County_Box
+            // 
+            this.County_Box.AutoSize = true;
+            this.County_Box.Location = new System.Drawing.Point(802, 306);
+            this.County_Box.Name = "County_Box";
+            this.County_Box.Size = new System.Drawing.Size(0, 16);
+            this.County_Box.TabIndex = 13;
+            // 
+            // City_Box
+            // 
+            this.City_Box.AutoSize = true;
+            this.City_Box.Location = new System.Drawing.Point(802, 268);
+            this.City_Box.Name = "City_Box";
+            this.City_Box.Size = new System.Drawing.Size(0, 16);
+            this.City_Box.TabIndex = 12;
+            // 
+            // Address_Box
+            // 
+            this.Address_Box.AutoSize = true;
+            this.Address_Box.Location = new System.Drawing.Point(802, 231);
+            this.Address_Box.Name = "Address_Box";
+            this.Address_Box.Size = new System.Drawing.Size(0, 16);
+            this.Address_Box.TabIndex = 11;
+            // 
+            // Name_Box
+            // 
+            this.Name_Box.AutoSize = true;
+            this.Name_Box.Location = new System.Drawing.Point(802, 193);
+            this.Name_Box.Name = "Name_Box";
+            this.Name_Box.Size = new System.Drawing.Size(0, 16);
+            this.Name_Box.TabIndex = 10;
+            // 
+            // searchbox
+            // 
+            this.searchbox.Location = new System.Drawing.Point(826, 99);
+            this.searchbox.Name = "searchbox";
+            this.searchbox.Size = new System.Drawing.Size(277, 22);
+            this.searchbox.TabIndex = 18;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(1007, 139);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.TabIndex = 19;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1293, 514);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.searchbox);
+            this.Controls.Add(this.Phone_Box);
+            this.Controls.Add(this.Phone1_Box);
+            this.Controls.Add(this.Zip_Box);
+            this.Controls.Add(this.State_Box);
+            this.Controls.Add(this.County_Box);
+            this.Controls.Add(this.City_Box);
+            this.Controls.Add(this.Address_Box);
+            this.Controls.Add(this.Name_Box);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label Phone_Box;
+        private System.Windows.Forms.Label Phone1_Box;
+        private System.Windows.Forms.Label Zip_Box;
+        private System.Windows.Forms.Label State_Box;
+        private System.Windows.Forms.Label County_Box;
+        private System.Windows.Forms.Label City_Box;
+        private System.Windows.Forms.Label Address_Box;
+        private System.Windows.Forms.Label Name_Box;
+        private System.Windows.Forms.TextBox searchbox;
+        private System.Windows.Forms.Button SearchButton;
+    }
+}
+
